14cbc46dbad508c127d02ba62fdaa75989ee37d3
Aki Koskinen <akikoskinen@yahoo.com>
2012-02-28 22:46:01 +0200

    Reads Jenkins RSS feed and changes the icon according to whether everything's OK or not
    The URL is hardcoded, as well as the refresh interval.

--- ciplasmoid/contents/ui/ciplasmoid.qml
+++ ciplasmoid/contents/ui/ciplasmoid.qml
@@ -1,5 +1,19 @@
 import QtQuick 1.0
+import org.kde.plasma.core 0.1 as PlasmaCore
+import "citools.js" as CITools
 
 Image {
-    source: "plasmapackage:/images/success.png"
+	id: icon
+	source: "plasmapackage:/images/success.png"
+
+	PlasmaCore.DataSource {
+		id: dataSource
+		engine: "rss"
+		connectedSources: ["http://localhost:2000/rssLatest"]
+		interval: 60 * 1000
+		
+		onNewData: {
+			CITools.handleItems(data["items"])
+		}
+	}
 }
---
+++ ciplasmoid/contents/ui/citools.js
@@ -0,0 +1,20 @@
+var OKCodes = ["stable", "back to normal"];
+
+function handleItems(items) {
+	var jenkinsTitleRE = /(.*) #[0-9]+ \((.*)\)/;
+	var allOK = true;
+	for (var i in items) {
+		var result = jenkinsTitleRE.exec(items[i].title);
+		if (result) {
+			if (OKCodes.indexOf(result[2]) == -1) {
+				allOK = false;
+			}
+		}
+	}
+	
+	if (allOK) {
+		icon.source = "plasmapackage:/images/success.png"
+	} else {
+		icon.source = "plasmapackage:/images/failure.png"
+	}
+}
